a8cc556ab9d2a7e082a944728f59b4cdb03a4b00
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2023-05-27 20:29:29 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,6 @@
-const { MongoClient } = require('mongodb');
+// utils/db.js
+
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
@@ -8,8 +10,12 @@ class DBClient {
 
     const url = `mongodb://${host}:${port}/${database}`;
 
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.db = null;
+    this.client = new MongoClient(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    this.connection = null;
 
     this.connect();
   }
@@ -17,10 +23,10 @@ class DBClient {
   async connect() {
     try {
       await this.client.connect();
-      this.db = this.client.db();
-      console.log('Database connected');
+      this.connection = this.client.db();
+      console.log('Connected to MongoDB');
     } catch (error) {
-      console.error('Error connecting to database:', error);
+      console.error('MongoDB connection error:', error);
     }
   }
 
@@ -29,47 +35,13 @@ class DBClient {
   }
 
   async nbUsers() {
-    try {
-      const count = await this.db.collection('users').countDocuments();
-      return count;
-    } catch (error) {
-      console.error('Error retrieving user count:', error);
-      return 0;
-    }
+    return this.connection.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    try {
-      const count = await this.db.collection('files').countDocuments();
-      return count;
-    } catch (error) {
-      console.error('Error retrieving file count:', error);
-      return 0;
-    }
-  }
-
-  async createUser(email, password) {
-    try {
-      const user = { _id: new ObjectId(), email, password };
-      const result = await this.db.collection('users').insertOne(user);
-      return result.ops[0];
-    } catch (error) {
-      console.error('Error creating user:', error);
-      return null;
-    }
-  }
-
-  async getUserByEmail(email) {
-    try {
-      const user = await this.db.collection('users').findOne({ email });
-      return user;
-    } catch (error) {
-      console.error('Error retrieving user by email:', error);
-      return null;
-    }
+    return this.connection.collection('files').countDocuments();
   }
 }
 
 const dbClient = new DBClient();
-
-module.exports = dbClient;
+export default dbClient;
