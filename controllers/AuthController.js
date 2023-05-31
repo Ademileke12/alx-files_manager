a71ecc3cb371ecf0aaa8a5acfa1a8101f97beb2c
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2023-05-31 19:54:46 +0100

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,66 +1,20 @@
-const { v4: uuidv4 } = require('uuid');
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+/* eslint-disable import/no-named-as-default */
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
 
-class AuthController {
+export default class AuthController {
   static async getConnect(req, res) {
-    const authHeader = req.header('Authorization');
+    const { user } = req;
+    const token = uuidv4();
 
-    if (!authHeader || !authHeader.startsWith('Basic ')) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const credentials = Buffer.from(authHeader.slice(6), 'base64').toString();
-    const [email, password] = credentials.split(':');
-
-    if (!email || !password) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    try {
-      const collection = dbClient.client.db().collection('users');
-
-      const user = await collection.findOne({ email, password: sha1(password) });
-
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      const token = uuidv4();
-      const key = `auth_${token}`;
-      const userId = user._id.toString();
-
-      await redisClient.client.setex(key, 86400, userId);
-
-      return res.status(200).json({ token });
-    } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+    res.status(200).json({ token });
   }
 
   static async getDisconnect(req, res) {
-    const token = req.header('X-Token');
+    const token = req.headers['x-token'];
 
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    try {
-      const key = `auth_${token}`;
-
-      const deletedCount = await redisClient.client.del(key);
-
-      if (deletedCount === 0) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      return res.status(204).end();
-    } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+    await redisClient.del(`auth_${token}`);
+    res.status(204).send();
   }
 }
-
-module.exports = AuthController;
