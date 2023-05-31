9ba97a51f04fa7edbd8bac482b23e6541f0b1a99
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2023-05-31 17:12:21 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 // GET /status
 router.get('/status', AppController.getStatus);
@@ -21,5 +22,8 @@ router.get('/disconnect', AuthController.getDisconnect);
 
 // GET /users/me
 router.get('/users/me', UsersController.getMe);
+
+// POST /files
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
